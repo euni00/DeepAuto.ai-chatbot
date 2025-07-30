0a3596c6a12932a434760528cd319205b22a0020
euni00 <amy000809@gmail.com>
2025-07-30 23:39:20 +0900

    💄 Style: add 'New Chat' and 'Search Chat' button icons

--- nextjs/src/common/Layout/SideBar.tsx
+++ nextjs/src/common/Layout/SideBar.tsx
@@ -5,7 +5,7 @@ import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
 import { useGetChatSessionsQuery } from '@/features/chat/useChatService';
 import { useAtom } from 'jotai';
-import { sessionAtom } from '../store/session';
+import { sessionAtom } from '../store/chatStore';
 
 export default function SideBar() {
   const chatSessions = useGetChatSessionsQuery();
@@ -20,16 +20,48 @@ export default function SideBar() {
         </div>
         <Button
           variant="ghost"
-          className="justify-start w-full gap-3 text-zinc-200 hover:bg-zinc-800 hover:text-white"
+          className="justify-start w-full gap-3 text-zinc-200 hover:bg-zinc-800 hover:text-white flex items-center cursor-pointer"
           onClick={() => setSessionId(null)}
         >
-          New Chat
+          <span className="mr-1 flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+              />
+            </svg>
+          </span>
+          새 채팅
         </Button>
         <Button
           variant="ghost"
-          className="justify-start w-full gap-3 text-zinc-200 hover:bg-zinc-800 hover:text-white"
+          className="justify-start w-full gap-3 text-zinc-200 hover:bg-zinc-800 hover:text-white flex items-center cursor-pointer"
         >
-          Search Chat
+          <span className="mr-1 flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+              />
+            </svg>
+          </span>
+          채팅 검색
         </Button>
       </div>
       <Separator className="bg-zinc-800" />
